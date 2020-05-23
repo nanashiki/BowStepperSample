32d31637df2761fb1ca2ee3a4cce1269e075670c
nanashiki <nanashiki.app@gmail.com>
2020-05-23 22:02:07 +0900

    Initial Commit

---
+++ BowStepperSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BowStepperSample
+//
+//  Created by nanashiki on 2020/05/23.
+//  Copyright © 2020 nanashiki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
